138bfcf8bd88f75ab7c027a885892a15390a2108
R. Faskhetdinov <cyrax.985@gmail.com>
2021-07-05 18:15:18 +0300

    Minor improvements

--- app/src/main/java/summerpractice/team7/mymemory/fragments/AllTasksFragment.kt
+++ app/src/main/java/summerpractice/team7/mymemory/fragments/AllTasksFragment.kt
@@ -1,22 +1,9 @@
 package summerpractice.team7.mymemory.fragments
 
-import android.os.Bundle
-//import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-
 import summerpractice.team7.mymemory.R
+import summerpractice.team7.mymemory.ui.base.BaseFragment
 
-class AllTasksFragment : Fragment() {
+class AllTasksFragment : BaseFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_tasks, container, false)
-    }
-
+    override val layout: Int = R.layout.fragment_tasks
 }

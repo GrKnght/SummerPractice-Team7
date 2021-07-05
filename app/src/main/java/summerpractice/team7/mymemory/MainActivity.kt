a65d0be4a62e044b745e2dc4929ddaef59ce7bc5
Bashir Galeev <bashir.2002@mail.ru>
2021-07-05 22:00:21 +0300

    All was modificated

--- app/src/main/java/summerpractice/team7/mymemory/MainActivity.kt
+++ app/src/main/java/summerpractice/team7/mymemory/MainActivity.kt
@@ -2,15 +2,10 @@ package summerpractice.team7.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import summerpractice.team7.mymemory.fragments.AchieveFragment
-import summerpractice.team7.mymemory.fragments.AllTasksFragment
-import summerpractice.team7.mymemory.fragments.DaylyTaskFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,47 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Все фрагменты
-        val achieveFragment = AchieveFragment() as Fragment
-        val allTasksFragment = AllTasksFragment() as Fragment
-        val daylyTaskFragment = DaylyTaskFragment() as Fragment
-        val bottom1: BottomNavigationView = findViewById(R.id.bottom1)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
 
-        //Атрибуты, включащиеся, когда во фрагменте ничего нет
-        val smthNoText: TextView = findViewById(R.id.smthNoText)
-        val smthNoIcon: ImageView = findViewById(R.id.noTasksIcon)
+        bottomNavigationView.setupWithNavController(navController)
 
-        makeFragment(daylyTaskFragment)
-        if(smthHas) {
-            smthNoIcon.visibility = View.GONE
-            smthNoText.visibility = View.GONE
-        }
-        smthNoText.setText("У вас сегодня нет дел")
-
-        bottom1.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.dayly_tasks_icon -> {
-                    makeFragment(daylyTaskFragment)
-                    smthNoText.setText("У вас сегодня нет дел")
-                }
-                R.id.achievement_icon -> {
-                    makeFragment(achieveFragment)
-                    smthNoText.setText("У вас нет достижений")
-                }
-                R.id.all_tasks_icon -> {
-                    makeFragment(allTasksFragment)
-                    smthNoText.setText("У вас нет списка задач")
-                }
-            }
-            true
-        }
-
-    }
-
-    private fun makeFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_wrapper, fragment)
-            commit()
-        }
     }
 }
--- app/src/main/java/summerpractice/team7/mymemory/fragments/AllTasksFragment.kt
+++
@@ -1,9 +0,0 @@
-package summerpractice.team7.mymemory.fragments
-
-import summerpractice.team7.mymemory.R
-import summerpractice.team7.mymemory.ui.base.BaseFragment
-
-class AllTasksFragment : BaseFragment() {
-
-    override val layout: Int = R.layout.fragment_tasks
-}

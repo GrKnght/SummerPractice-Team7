afa533e7b82c66c05c354b40c5e1fb766f8c55c4
Bashir Galeev <bashir.2002@mail.ru>
2021-07-05 15:16:41 +0300

    Added some modifications

--- app/src/main/java/summerpractice/team7/mymemory/MainActivity.kt
+++ app/src/main/java/summerpractice/team7/mymemory/MainActivity.kt
@@ -2,49 +2,66 @@ package summerpractice.team7.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import summerpractice.team7.mymemory.fragments.AchieveFragment
 import summerpractice.team7.mymemory.fragments.AllTasksFragment
 import summerpractice.team7.mymemory.fragments.DaylyTaskFragment
 
 class MainActivity : AppCompatActivity() {
+
+    var smthHas: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val achieveFragment = AchieveFragment()
-        val allTasksFragment = AllTasksFragment()
-        val daylyTaskFragment = DaylyTaskFragment()
+
+        // Все фрагменты
+        val achieveFragment = AchieveFragment() as Fragment
+        val allTasksFragment = AllTasksFragment() as Fragment
+        val daylyTaskFragment = DaylyTaskFragment() as Fragment
         val bottom1: BottomNavigationView = findViewById(R.id.bottom1)
 
-        makeAchieveFragment(achieveFragment)
+        //Атрибуты, включащиеся, когда во фрагменте ничего нет
+        val smthNoText: TextView = findViewById(R.id.smthNoText)
+        val smthNoIcon: ImageView = findViewById(R.id.noTasksIcon)
+
+        makeFragment(daylyTaskFragment)
+        if(smthHas) {
+            smthNoIcon.visibility = View.GONE
+            smthNoText.visibility = View.GONE
+        }
+        smthNoText.setText("У вас сегодня нет дел")
 
         bottom1.setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.dayly_tasks_icon -> makeDaylyTasksFragment(daylyTaskFragment)
-                R.id.achievement_icon -> makeAchieveFragment(achieveFragment)
-                R.id.all_tasks_icon -> makeAllTaskFragment(allTasksFragment)
+                R.id.dayly_tasks_icon -> {
+                    makeFragment(daylyTaskFragment)
+                    smthNoText.setText("У вас сегодня нет дел")
+                }
+                R.id.achievement_icon -> {
+                    makeFragment(achieveFragment)
+                    smthNoText.setText("У вас нет достижений")
+                }
+                R.id.all_tasks_icon -> {
+                    makeFragment(allTasksFragment)
+                    smthNoText.setText("У вас нет списка задач")
+                }
             }
             true
         }
 
     }
 
-    private fun makeAchieveFragment(fragment: AchieveFragment) =
+    private fun makeFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fl_wrapper, fragment)
             commit()
         }
-
-    private fun makeAllTaskFragment(fragment: AllTasksFragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_wrapper, fragment)
-            commit()
-        }
-
-    private fun makeDaylyTasksFragment(fragment: DaylyTaskFragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_wrapper, fragment)
-            commit()
-        }
+    }
 }
--- app/src/main/java/summerpractice/team7/mymemory/fragments/DaylyTaskFragment.kt
+++ app/src/main/java/summerpractice/team7/mymemory/fragments/DaylyTaskFragment.kt
@@ -5,15 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import summerpractice.team7.mymemory.R
 
 class DaylyTaskFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dayly_task, container, false)
     }

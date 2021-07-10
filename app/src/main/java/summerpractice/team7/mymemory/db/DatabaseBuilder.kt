741305f8fdd52287179b44d11f8ee1097c1a0c20
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2021-07-10 18:59:53 +0300

    Move database initializer from MainActivity over to DatabaseBuilder

--- app/src/main/java/summerpractice/team7/mymemory/db/DatabaseBuilder.kt
+++ app/src/main/java/summerpractice/team7/mymemory/db/DatabaseBuilder.kt
@@ -2,6 +2,8 @@ package summerpractice.team7.mymemory.db
 
 import android.app.Activity
 import androidx.room.*
+import android.util.Log
+import summerpractice.team7.mymemory.util.AchievementHelper
 
 class DatabaseBuilder {
 
@@ -17,4 +19,32 @@ class DatabaseBuilder {
     // val inProgressTasks: List<Task> = taskDao.getTasksWithStatus(TaskStatus.InProgress)
     // val tasks: List<Task> = taskDao.getAll()
     // val updatedTasks: List<Task> = taskDao.updateOutdatedStatuses()
+
+    fun initDB(activity: Activity): MyMEMoryDB {
+        val db = createDBInstance(activity)
+        val achievementDao = db.achievementDao()
+        val userAchievementsCount = achievementDao.getAll().count()
+        val defaultAchievementsCount = AchievementHelper(achievementDao).defaultAchievementListCount()
+        if (userAchievementsCount == 0) {
+            AchievementHelper(achievementDao).initializeDB()
+        } else if (userAchievementsCount != defaultAchievementsCount) {
+            Log.d("MainActivity.initDB","User has different achievements, reinitializing...")
+            Log.d("MainActivity.initDB","User: $userAchievementsCount | Default: $defaultAchievementsCount")
+            val unlockedTasks = AchievementHelper(achievementDao).getUnlocked()
+            achievementDao.destroyTable()
+            AchievementHelper(achievementDao).initializeDB()
+            for (task in unlockedTasks) {
+                try {
+                    achievementDao.unlockById(task.id, task.unlockedAt)
+                    Log.d("MainActivity.initDB","- Migrated task id ${task.id}")
+                } catch(e: Exception) {
+                    Log.d("MainActivity.initDB","- Ignored task id ${task.id} (no longed exists)")
+                }
+            }
+            Log.d("MainActivity.initDB","Successfully migrated achievements table")
+        } else {
+            Log.d("MainActivity.initDB","No achievement migration needed")
+        }
+        return db
+    }
 }

5a5e4db3bb9a1def14f9581623a1151f0650523b
Bashir Galeev <bashir.2002@mail.ru>
2021-07-11 13:27:35 +0300

    DB works, but not right!

--- app/src/main/java/summerpractice/team7/mymemory/ui/main/fragment/AchievementFragment.kt
+++ app/src/main/java/summerpractice/team7/mymemory/ui/main/fragment/AchievementFragment.kt
@@ -6,19 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import summerpractice.team7.mymemory.R
-import summerpractice.team7.mymemory.databinding.FragmentAchievementBinding
 
 class AchievementFragment : Fragment() {
-
-    private var binding: FragmentAchievementBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        binding = FragmentAchievementBinding.inflate(inflater, container, false)
-        return binding?.root
+        return inflater.inflate(R.layout.fragment_achievement, container, false)
     }
-
-
 
 }
